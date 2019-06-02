2224b8c95f95b4b9200f36052d8a1882f8daa699
ultraviolet70 <ektrinakretova@gmail.com>
2019-06-02 22:49:05 +0300

    02.06.2019

--- Shop/FormScreen.Designer.cs
+++ Shop/FormScreen.Designer.cs
@@ -58,7 +58,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(178)))), ((int)(((byte)(80)))));
-            this.label1.Location = new System.Drawing.Point(23, -2);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(667, 67);
             this.label1.TabIndex = 2;
